e02921d17c89af380c6ba8f4328f9c138124efd6
Chang <changleayou3@gmail.com>
2026-03-10 12:30:18 +0100

    add inverted index

--- search.py
+++ search.py
@@ -1,37 +1,36 @@
 import string
 
 with open("text.txt", "r") as f:
-   text = f.read()
+   lines = f.readlines()
 
-words = text.split()
+for line_number, line in enumerate(lines):
+   print(line_number, line)
 
-count = {}
-for word in words:
-   word = word.strip(string.punctuation)
-   word = word.lower()
+index = {}
+for line_number, line in enumerate(lines):
+   words = line.split()
+   for word in words:
+      word = word.strip(string.punctuation)
+      word = word.lower()
 
-   if word in count:
-      count[word] += 1
-   else:
-      count[word] = 1
+      if word in index:
+         if line_number not in index[word]:
+            index[word].append(line_number)
+      else:
+         index[word] = [line_number]
 
-print(count)
+print(index)
 
-
-print("Which word you want to count?")
+print("Which word you want to know its row number?")
 print("Enter 'quit' if you want to quit.")
 
 user_input = input()
 
 while (user_input != "quit"):
 
-   if user_input in count:
-      print(count[user_input])
+   if user_input in index:
+      print(index[user_input])
    else:
       print("There is no such word.")
 
    user_input = input()
-
-
-
-
